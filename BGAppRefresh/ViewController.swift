d09a7dc2b626569cb8ed1730c2fbc753b73c76de
Robert Ryan <robert.ryan@mindspring.com>
2020-05-28 14:37:21 -0700

    Initial Commit

---
+++ BGAppRefresh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BGAppRefresh
+//
+//  Created by Robert Ryan on 5/28/20.
+//  Copyright © 2020 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
